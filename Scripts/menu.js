43e8aa110ddf0cfd654d70f75da16fa8d0ea88df
facundo thomas ramirez <facundorz2003@gmail.com>
2025-07-14 20:19:25 -0300

    animaciones del nav modificadas

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -6,11 +6,15 @@ let visible = false;
 boton.addEventListener("click", () => {
     visible = !visible;
 
-    if(visible){
-        nav.classList.remove("translate-x-full");
-        nav.classList.add("translate-x-0");
-    }else{
-        nav.classList.remove("translate-x-0");
-        nav.classList.add("translate-x-full");
+    if (visible) {
+        nav.classList.remove("hidden", "animate-fade-out-up");
+        nav.classList.add("animate-fade-in-down");
+    } else {
+        nav.classList.remove("animate-fade-in-down");
+        nav.classList.add("animate-fade-out-up");
+
+        setTimeout(() => {
+        nav.classList.add("hidden");
+        }, 500);
     }
 });
